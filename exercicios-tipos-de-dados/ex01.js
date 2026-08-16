e5a0519e59927aee4968ecf9edcaf03fa3059a27
Elisa Ranikelly <elisaranikelly2004@gmail.com>
2026-08-16 17:34:21 -0300

    feat: adição dos exercícios de tipos de dados

---
+++ exercicios-tipos-de-dados/ex01.js
@@ -0,0 +1,7 @@
+// EXERCÍCIOS SOBRE STRINGS
+
+var nome1 = "Elisa";
+var nome2 = 'Teté';
+var nome3 = `Wallasson`;
+
+console.log("Olá " + nome1 + ", " + nome2 + " e " + nome3 + "!");
---
+++ exercicios-tipos-de-dados/ex04.js
@@ -0,0 +1,8 @@
+//AND
+console.log(10  == 10 && 10 > 7);
+
+//OR
+console.log(4 <= 2 || "Elisa" == "Helisa1");
+
+//NOT
+console.log(!(10 > 7));
