37dde3c5c574ff1f7f86e5fda72acd9b02f7d8bb
Elisa Ranikelly <elisaranikelly2004@gmail.com>
2026-08-17 14:30:37 -0300

    feat: adição dos exericios sobre estrutura

---
+++ exercicios-estrutura-de-programacao/ex01.js
@@ -0,0 +1,7 @@
+var string = "Elisa";
+var number = 10;
+var boolean = true;
+
+console.log("O tipo da variável string é: " + typeof string);
+console.log("O tipo da variável number é: " + typeof number);
+console.log("O tipo da variável boolean é: " + typeof boolean);
